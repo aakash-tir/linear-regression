97809fc570e60f783e1541211fbc79ad04181745
aakash-tir <aakashtirathdas@gmail.com>
2025-05-22 02:23:31 -0700

    created a dataset to use

---
+++ create_data.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+#set seed
+np.random.seed(369)
+
+# Generate 100 x values between 0 and 20
+x = np.linspace(0, 20, 100)
+
+#define function
+noise = np.random.normal(0, 2, size=x.shape)  # mean=0, std=2
+y = np.round(np.random.uniform(0, 10), 4) * x + np.round(np.random.uniform(0, 10), 4) + noise
+
+#dataframe creation
+df = pd.DataFrame({'x': x, 'y': y})
+
+#save as csv
+df.to_csv('linear_data.csv', index=False)
+
+print("Dataset saved to 'linear_data.csv'")
